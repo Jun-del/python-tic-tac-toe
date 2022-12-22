df8ab4b90e819dba6b79a7e5b3bc034ea411f7b1
Jun <neohwj28@gmail.com>
2022-12-22 19:04:53 +0800

    Add graphical user interface file
    Add graphical user interface file.

--- gui.py
+++ gui.py
@@ -1,1 +1,18 @@
 import tkinter as tk
+
+class Game:
+    def __init__(self):
+        # Create a window
+        self.window = tk.Tk()
+        # Set the title of the window as Tic Tac Toe
+        self.window.title("Tic Tac Toe")
+        # Set the size of the window
+        self.window.geometry("400x400")
+        # Disable the ability to resize the window
+        self.window.resizable(0, 0)
+        # Set the background color of the window
+        self.window.configure(bg="white")
+        # Start the window
+        self.window.mainloop()
+
+        
