ea13003db7df0f79e8d8ebb536cbf2a129013d06
Jun <neohwj28@gmail.com>
2023-01-03 10:20:23 +0800

    Add board grids and buttons to the window

--- gui.py
+++ gui.py
@@ -20,3 +20,28 @@ class TicTacToeBoard(tk.TK):
             font = font.Font(size = 28, weight = "bold"),
         ), # Create a label to display the game status
         self.display.pack() # Pack (display) the label into the frame
+
+    def board_grid(self):
+        grid_frame = tk.Frame(master = self) # Create a frame to hold the game's grif of cells/buttons
+        grid_frame.pack() # Place the frame into the window
+        for row in range(3):
+            self.rowconfigure(row, weight = 1, minsize = 50) # Configure the row to be 50 pixels tall
+            self.columnconfigure(row, weight = 1, minsize = 75) # Configure the column to be 50 pixels wide
+            for column in range(3):
+                button = tk.Button(
+                    master = grid_frame,
+                    text = "",
+                    font = font.Font(size = 36, weight = "bold"),
+                    fg = "black",
+                    width = 3,
+                    height = 2,
+                    highlightbackground = "lightblue"
+                ) # Create a button for every cell in the grid
+                self._cells[button] = (row, column) # Add the button to the dictionary of cells
+                button.grid(
+                    row = row,
+                    column = column,
+                    padx = 5,
+                    pady = 5,
+                    sticky = "nsew" # The sticky parameter tells the button to expand to fill the entire cell
+                ) # Place the button into the grid
