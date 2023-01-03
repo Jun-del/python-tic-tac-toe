ceb2c84468ff17cdb89a8fdb38a6ad87359663bb
Jun <neohwj28@gmail.com>
2023-01-03 10:05:21 +0800

    Add display board class

--- gui.py
+++ gui.py
@@ -11,4 +11,12 @@ class TicTacToeBoard(tk.TK):
         self.title("Tic-Tac-Toe") # Set the title of the window
         self._cells = {} # Dictionary of or cells (row, column) -> button
 
-   
+    def board_display(self):
+        display_frame = tk.Frame(master = self) # Create a frame to hold the board
+        display_frame.pack(fill = tk.x) # The fill parameter tells the frame to fill the entire width of the window
+        self.display = tk.Label(
+            master = display_frame, # The label is a child of the master frame
+            text = "Ready to play!",
+            font = font.Font(size = 28, weight = "bold"),
+        ), # Create a label to display the game status
+        self.display.pack() # Pack (display) the label into the frame
